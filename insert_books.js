be81db2d6a5af2283467fab2823c6cab33f94d3a
samuel-wambugu <gichehasamuel555@gmail.com>
2025-06-04 13:37:49 +0300

    Mongodb

--- insert_books.js
+++ insert_books.js
@@ -164,7 +164,114 @@ async function insertBooks() {
     const insertedBooks = await collection.find({}).toArray();
     insertedBooks.forEach((book, index) => {
       console.log(`${index + 1}. "${book.title}" by ${book.author} (${book.published_year})`);
+
+    
     });
+    console.log("\n----------------- ");
+    console.log("the books by adventure are : ")
+    const genrebook = await collection.find({genre : "Adventure"}).toArray();
+    genrebook.forEach((books,indexs) => {
+        console.log(`${indexs + 1}. "${books.title}" by ${books.author} (${books.published_year})`);
+    });
+
+    console.log("\n----------------- ");
+    console.log("the books published after 1888 : ")
+    const yearbooks = await collection.find({published_year: {$gt: 1888}}).toArray();
+    yearbooks.forEach((books,indexs) => {
+        console.log(`${indexs + 1}. "${books.title}" by ${books.author} (${books.published_year})`);
+    });
+
+    console.log("\n----------------- ");
+    console.log("the books published by Herman Melville : ")
+    const authorbooks = await collection.find({author: "Herman Melville"}).toArray();
+    authorbooks.forEach((books,indexs) => {
+        console.log(`${indexs + 1}. "${books.title}" by ${books.author} (${books.published_year})`);
+    });
+
+    console.log("\n----------------- ");
+    console.log("updating the price of a book : ")
+    collection.updateOne({published_year: 1847 }, {$set : {price : 20}});
+    const pricebooks = await collection.find({published_year: 1847}).toArray();
+    pricebooks.forEach((books,indexs) => {
+        console.log(`${indexs + 1}. "${books.title}" new price is: ${books.price}`);
+    });
+    collection.deleteOne({title: 'The Alchemist'});
+    
+    console.log("\n----------------- ");
+    console.log("books in stock and produced after 2010: ")
+    const stockbooks = await collection.find({$and : [{in_stock : true},{published_year:{$gte: 1988}}]}).toArray();
+    stockbooks.forEach((books,indexs) => {
+        console.log(`${indexs + 1}. "${books.title}" by ${books.author} (${books.published_year})`);
+    });
+
+    console.log("\n----------------- ");
+    console.log("giving the title ,the author and the price : ")
+    const projectionbooks = await collection.find({},{projection: {title: 1, author:1, price: 1, _id:0}}).toArray();
+    console.log(projectionbooks);
+
+    console.log("\n----------------- ");
+    console.log("sorting the bookes by price by ascending order : ")
+    const ascebooks = await collection.find().sort({price:1}).toArray();
+    const descebooks = await collection.find().sort({price: -1}).toArray();
+    console.log(ascebooks);
+
+    console.log("\n----------------- ");
+    console.log("sorting the bookes by price by descending order : ");
+    console.log(descebooks);
+
+    const pageNumber = 1;
+    console.log("\n----------------- ");
+    console.log("Use the `limit` and `skip` methods to implement pagination (5 books per page)");
+    const pagebooks = await collection.find().skip((pageNumber - 1) * 5).limit(5).toArray();
+    pagebooks.forEach((books, indexs) => {
+      console.log(`${indexs + 1}. "${books.title}" by ${books.author} (${books.published_year})`);
+    });
+
+    const avgprice = await collection.aggregate([{$match : {genre: 'Fiction'}},{$group: {_id: null, avarengeprice:{$avg: '$price'}}}]).toArray();
+    console.log("the averange price is ", avgprice[0]?.avarengeprice);
+    
+    console.log("\nthe author with the most books");
+    const topauthor = await collection.aggregate([{$group: {_id: "$author", bookcount: {$sum : 1}}},{$limit: 1}]).toArray();
+    if (topauthor.length > 0) {
+      console.log(`Author with most books: ${topauthor[0]._id}(${topauthor[0].bookcount} books)`);
+    }else {
+      console.log("No authors found.");
+    } 
+
+    const booksByDecade = await collection.aggregate([
+        {
+          $project: {
+            decade: {
+              $multiply: [
+                { $floor: { $divide: ["$published_year", 10] } },
+                10
+              ]
+            }
+          }
+        },
+        {
+          $group: {
+            _id: "$decade",
+            count: { $sum: 1 }
+          }
+        },
+        {
+          $sort: { _id: 1 } 
+        }
+      ]).toArray();
+
+      booksByDecade.forEach(group => {
+        console.log(`Decade: ${group._id}s — ${group.count} books`);
+      });
+      const query = { author: "Paulo Coelho", published_year: 1987 };
+
+      const explainWithoutIndex = await collection.find(query).explain("executionStats");
+      console.log("Without compound index:", explainWithoutIndex.executionStats);
+
+      const explainWithIndex = await collection.find(query).explain("executionStats");
+      console.log("With compound index:", explainWithIndex.executionStats);
+
+
 
   } catch (err) {
     console.error('Error occurred:', err);
